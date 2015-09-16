ea181fd93e75fcd623b3c6cc701746286dea2f96
MissionHealth <missionhealth@MissionHealthdeMac-mini.local>
2015-09-16 18:02:10 +0800

    remove printf

--- rpi_gpio.c
+++ rpi_gpio.c
@@ -1,27 +1,3 @@
-/*
- * blink8.c:
- *	Simple sequence over the first 8 GPIO pins - LEDs
- *	Aimed at the Gertboard, but it's fairly generic.
- *
- * Copyright (c) 2012-2013 Gordon Henderson. <projects@drogon.net>
- ***********************************************************************
- * This file is part of wiringPi:
- *	https://projects.drogon.net/raspberry-pi/wiringpi/
- *
- *    wiringPi is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    wiringPi is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
- */
 
 #include <stdio.h>
 #include <wiringPi.h>
@@ -49,13 +25,9 @@ int main (void)
     pinMode(i, BUZZER_GPIO_NUM);
 
     for (;;) {
-        time(&now);
 
+        time(&now);
         struct tm *current_time = gmtime(&now);
-        printf("time h = %d, m = %d, s = %d\n",
-               current_time->tm_hour,
-               current_time->tm_min,
-               current_time->tm_sec);
 
         // 6:00 - 22:00
         if (current_time->tm_hour >= 6 && current_time->tm_hour <= 22) {
